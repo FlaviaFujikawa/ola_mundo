24a5140548c6937f2ce3dd34e155adfa659cc21b
FlaviaFujikawa <125517971+FlaviaFujikawa@users.noreply.github.com>
2023-03-02 00:31:06 +0000

    inserindo variavel e input

---
+++ aula0103variavel.py
@@ -0,0 +1,4 @@
+variavel_nome = input("Escreva o nome: ")
+print("Meu nome é " + variavel_nome)
+variavel_idade = input("Escreva a idade: ")
+print("O(a) " + variavel_nome + " possui " + variavel_idade + " anos ")
