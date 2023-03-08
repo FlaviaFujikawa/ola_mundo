b2355dab3344a3a0e578c1104fd9794036c5f104
FlaviaFujikawa <125517971+FlaviaFujikawa@users.noreply.github.com>
2023-03-08 22:35:10 +0000

    aprendi variavel

---
+++ variavelvideo.py
@@ -0,0 +1,10 @@
+variavel_a = 20
+variavel_b = 5  
+meu_nome = "Flávia"
+
+
+print(variavel_a+variavel_b)
+print(variavel_a-variavel_b)
+print(variavel_a*variavel_b)
+print(variavel_a/variavel_b)
+print("Meu nome é " +  meu_nome)
