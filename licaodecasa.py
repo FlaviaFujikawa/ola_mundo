d3b084e5d8c936a4a9be859146233449cd9406a9
FlaviaFujikawa <125517971+FlaviaFujikawa@users.noreply.github.com>
2023-03-08 23:25:55 +0000

    ajuda do igor

---
+++ licaodecasa.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+endereco = input("Digite seu endereco: ")
+profissao = input("Digite sua profissao: ")
+#print("Nome: " + nome = "Endereço: " + "Profissão: " + profissao) - codigo simples
+print(f"Nome: {nome} -  Endereço: {endereco} - Profissão: {profissao}") 
